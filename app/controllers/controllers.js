1b5910694db3917391d5888dd80b406315ce3848
Taylor Hamling <0603671@live.com>
2015-01-09 14:02:42 +1300

    working on standard view. pagination completed with ability to filter, order etc.. Created all correct fields for the app (only for the chemical request screen I have). Need to finalize layout and make sure everything it working correctly.

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,64 +1,118 @@
-﻿/*#######################################################################
-  
-  Dan Wahlin
-  http://twitter.com/DanWahlin
-  http://weblogs.asp.net/dwahlin
-  http://pluralsight.com/training/Authors/Details/dan-wahlin
 
-  Normally like the break AngularJS controllers into separate files.
-  Kept them together here since they're small and it's easier to look through them.
-  example. 
-
-  #######################################################################*/
-
-
-//This controller retrieves data from the customersService and associates it with the $scope
-//The $scope is ultimately bound to the customers view
 app.controller('MainController', function ($scope, requestsService) {
 
-    //I like to have an init() for controllers that need to perform some initialization. Keeps things in
-    //one place...not required though especially in the simple example below
+
     init();
 
     function init() {
+        
+        $scope.currentPage = 1;
+        $scope.pageSize = 5;        
+        
         $scope.requests = requestsService.getRequests();
+
+      
     }
 
-    $scope.insertRequest = function () {
+    $scope.reorderRequest = function (id) {
+        for (var x in $scope.requests){
+            if ($scope.requests[x].id === id){
+                $scope.newRequest.firstName = $scope.requests[x].firstName;
+                $scope.newRequest.lastName = $scope.requests[x].lastName;
+                $scope.newRequest.city = $scope.requests[x].city;                
+            }
+        }
+    }
+
+
+
+
+    $scope.editRequest = function () {
         var firstName = $scope.newRequest.firstName;
         var lastName = $scope.newRequest.lastName;
         var city = $scope.newRequest.city;
-        requestsService.insertRequest(firstName, lastName, city);
-        $scope.newRequest.firstName = '';
-        $scope.newRequest.lastName = '';
-        $scope.newRequest.city = '';
-    };
+        requestsService.updateRequest(firstName, lastName, city);
+    }
+
+
+
 
     $scope.deleteRequest = function (id) {
         requestsService.deleteRequest(id);
     };
 });
 
-//This controller retrieves data from the customersService and associates it with the $scope
-//The $scope is bound to the order view
+
 app.controller('MainViewController', function ($scope, $routeParams, requestsService) {
     $scope.request = {};
     $scope.ordersTotal = 0.00;
 
-    //I like to have an init() for controllers that need to perform some initialization. Keeps things in
-    //one place...not required though especially in the simple example below
+
+
     init();
 
     function init() {
-        //Grab customerID off of the route        
-        var requestID = ($routeParams.requestID) ? parseInt($routeParams.requestID) : 0;
+    
+        var requestID = ($routeParams.reqID) ? parseInt($routeParams.reqID) : 0;
         if (requestID > 0) {
+
             $scope.request = requestsService.getRequest(requestID);
         }
     }
 
 });
 
+
+
+
+app.controller('ModalCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+  
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: '/app/partials/create.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items, requestsService) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+    $scope.insertRequest = function () {      
+
+        requestsService.insertRequest($scope.newRequest);
+        $scope.newRequest = {};
+    };
+    
+    
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
 
 
 
